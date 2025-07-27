6b7f4e6790f7992e2a70130dfa5bb6c60b0ac9d8
joojdev <jvpiovezan2905@gmail.com>
2025-07-27 17:30:28 -0300

    Added script to populate database automatically

---
+++ backend/src/scripts/populate_db.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "../../generated/prisma";
+
+
+(async () => {
+  const prisma = new PrismaClient();
+  await prisma.$connect();
+
+  await prisma.api.createMany({
+    data: [
+      {
+        name: 'OpenAI API',
+        domain: 'api.openai.com',
+        endpoint: '/',
+        protocol: 'https',
+        accessInterval: 5,
+      },
+      {
+        name: 'Google No Content',
+        domain: 'www.google.com',
+        endpoint: '/generate_204',
+        protocol: 'https',
+        accessInterval: 5,
+      },
+      {
+        name: 'AWS IP Ranges',
+        domain: 'ip-ranges.amazonaws.com',
+        endpoint: '/ip-ranges.json',
+        protocol: 'https',
+        accessInterval: 5,
+      }
+    ]
+  });
+
+  await prisma.$disconnect();
+})();
