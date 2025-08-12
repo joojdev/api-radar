37b2cbb39b0709a6466e358e404acb5d39921aab
joojdev <jvpiovezan2905@gmail.com>
2025-08-12 01:49:26 -0300

    Add: autoreload api data system

--- backend/src/plugins/socket.io.ts
+++ backend/src/plugins/socket.io.ts
@@ -1,41 +1,56 @@
-import fp from 'fastify-plugin';
-import { FastifyPluginAsync } from 'fastify';
-import { Server as SocketIOServer, Socket } from 'socket.io';
+import fp from "fastify-plugin";
+import { FastifyPluginAsync } from "fastify";
+import { Server as SocketIOServer, Socket } from "socket.io";
 
-declare module 'fastify' {
+declare module "fastify" {
   interface FastifyInstance {
-    io: SocketIOServer
+    io: SocketIOServer;
   }
 }
 
-const socketIOPlugin: FastifyPluginAsync = fp(async (fastify) => {
-  const io = new SocketIOServer(fastify.server, {
-    cors: {
-      origin: '*',
-      methods: ['GET', 'POST']
-    }
-  });
+function leaveAllRooms(socket: Socket) {
+  for (const roomName of socket.rooms) {
+    socket.leave(roomName);
+  }
+}
 
-  fastify.decorate('io', io);
-
-  io.on('connection', (socket: Socket) => {
-    console.log(`Socket connected: ${socket.id}`);
-
-    socket.on('disconnect', () => {
-      console.log(`Socket disconnected: ${socket.id}`);
+const socketIOPlugin: FastifyPluginAsync = fp(
+  async (fastify) => {
+    const io = new SocketIOServer(fastify.server, {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+      },
     });
 
-    socket.on('message', (message) => {
-      console.log(`Received message: ${message}`);
-    });
-  });
+    fastify.decorate("io", io);
 
-  fastify.addHook('onClose', (_, done) => {
+    io.on("connection", (socket: Socket) => {
+      console.log(`Socket connected: ${socket.id}`);
+
+      socket.on("disconnect", () => {
+        console.log(`Socket disconnected: ${socket.id}`);
+      });
+
+      socket.on("joinRoom", (roomName) => {
+        console.log("Putting socket", socket.id, "into room", roomName + "...");
+        leaveAllRooms(socket);
+        socket.join(roomName);
+      });
+
+      socket.on("leaveRoom", () => {
+        leaveAllRooms(socket);
+      });
+    });
+
+    fastify.addHook("onClose", (_, done) => {
       io.close();
       done();
     });
-}, {
-  name: 'socket-io-plugin'
-});
+  },
+  {
+    name: "socket-io-plugin",
+  },
+);
 
 export default socketIOPlugin;
